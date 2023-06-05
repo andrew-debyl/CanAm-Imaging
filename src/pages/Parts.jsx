2df450af72872ab4441bcdb3626772ed50040f05
Andrew <andrewdebyl@gmail.com>
2023-06-05 11:23:17 -0400

    changes

--- src/pages/Parts.jsx
+++ src/pages/Parts.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 import "../styles/Parts.css";
+import { Link } from "react-router-dom";
 
 function Parts() {
   return (
     <div>
       <div className="parts">
-        <h1 className="parts--title">
-          Xerox Parts
-        </h1>
+        <h1 className="parts--title">Xerox Parts</h1>
         <hr className="parts--hr" />
         <p className="parts--para">
-        CanAm Imaging provides Xerox parts to the I.S.O and Self-Service marketplace for the United States and Canada. Whether you’re looking for new or used Xerox parts, you can count on CanAm Imaging for parts that are fully guaranteed and competitively priced to help you run your business better – and for less.
+          CanAm Imaging provides Xerox parts to the I.S.O and Self-Service
+          marketplace for the United States and Canada. Whether you’re looking
+          for new or used Xerox parts, you can count on CanAm Imaging for parts
+          that are fully guaranteed and competitively priced to help you run
+          your business better – and for less.
         </p>
         <p className="parts--para">
-        Get up to <b>75%</b> off a huge selection of 100% genuine Xerox parts and rest assured that we stand behind the products we sell, new and recycled. Decommissioned from working equipment, all recycled parts undergo a rigorous quality control process to ensure they’re in reliable working order prior to purchase.
+          Get up to <b>75%</b> off a huge selection of 100% genuine Xerox parts
+          and rest assured that we stand behind the products we sell, new and
+          recycled. Decommissioned from working equipment, all recycled parts
+          undergo a rigorous quality control process to ensure they’re in
+          reliable working order prior to purchase.
         </p>
-    
+        <h3 className="parts--title2">Interested in selling Xerox parts?</h3>
+        <Link to='/buyingorselling' className="parts--contact-link">
+          Click Here
+        </Link>
+        <p className="parts--contact">
+          <b>Phone:</b> <span className="red">+1 866-758-0007</span>
+        </p>
         <p className="parts--contact">
           <b>Email:</b>
           <span className="blue">
             <a href="mailto:tech@canamimaging.com" className="email">
               {" "}
-              tech@canamimaging.com
+              parts@canamimaging.com
             </a>
           </span>
         </p>
--- src/pages/Supplies.jsx
+++ src/pages/Supplies.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import '../styles/Supplies.css'
+import React from "react";
+import "../styles/Supplies.css";
+import { Link } from "react-router-dom";
 
 function Supplies() {
   return (
-    <div>Supplies</div>
-  )
+    <div className="supplies">
+      <h1 className="supplies--title">Supplies</h1>
+      <hr className="supplies--hr" />
+      <div className="supplies__body">
+        <h3 className="supplies__body--title">
+          We meet your print and copy needs for less
+        </h3>
+        <p className="supplies--para">
+          CanAm Imaging carries a wide range of supplies from various
+          manufacturers to help you meet your print and copy needs with ease. To
+          purchase supplies, simply complete and submit this{" "}
+          <Link to="/buyingorselling" className="supplies--link">
+            form
+          </Link>{" "}
+          and we’ll get back to you within 4 hours.
+        </p>
+        <h3 className="supplies__body--title">
+          Trade in surplus supplies for cash
+        </h3>
+        <p className="supplies--para">
+          Why let your surplus supplies take up valuable storage space when you
+          can trade them in for cash? Tell us what you’ve got and we’ll make you
+          an offer.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Supplies
+export default Supplies;
